9eb187dc83daae1c1b6329a43c0bde753913ea11
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2016-11-19 23:56:40 -0300

    added script to test all dataset and all structures

---
+++ src/script_all_tests.sh
@@ -0,0 +1,41 @@
+./ct_main ls /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main ls /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main ls /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main ls /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main ls /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main ls /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
+
+./ct_main bs /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main bs /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main bs /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main bs /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main bs /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main bs /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
+
+./ct_main wt /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main wt /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main wt /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main wt /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main wt /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main wt /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
+
+./ct_main wth /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main wth /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main wth /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main wth /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main wth /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main wth /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
+
+./ct_main gmr /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main gmr /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main gmr /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main gmr /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main gmr /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main gmr /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
+
+./ct_main ap /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
+./ct_main ap /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main ap /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main ap /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main ap /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
+./ct_main ap /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
