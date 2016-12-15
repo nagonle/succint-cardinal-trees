4af77d87ebe13a5f84ec2ed597bc6c471aa30508
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2016-12-14 23:23:03 -0300

    clean repo

--- src/get_voc.sh
+++
@@ -1,6 +0,0 @@
-./ct_main wth /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/English/englishDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
--- src/script_test_02.sh
+++
@@ -1,6 +0,0 @@
-./ct_main ls /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
-./ct_main ls /data/Trees/DFUDS/LZ78/English/englishDFUDS
-./ct_main ls /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
-./ct_main ls /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
-./ct_main ls /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
-./ct_main ls /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
