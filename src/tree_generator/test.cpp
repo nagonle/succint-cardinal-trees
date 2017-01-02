fccc6d24e798576a1a2ca3c20d52d271adc63c87
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2017-01-01 23:15:57 -0300

    update tree generator.

--- src/tree_generator/test.cpp
+++ src/tree_generator/test.cpp
@@ -1,103 +1,17 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-string multiply_str(string a, int n) {
-	string b = "";
-	for (size_t i=0; i<n; i++) b += a;
-	return b;
+void write_file(ofstream& out, size_t N) {
+	out << "hola";
+	if (N > 10) return;
+	N++;
+	write_file(out, N);
 }
 
-// max_arity: return the arity of a tree.
-template <class type>
-size_t get_max_arity(type * ct) {
-	size_t x;
-	size_t max_arity;
-	max_arity = ct->degree(1);
-	for (size_t i=1; i<ct->count_nodes(); i++) {
-		x = ct->tree_select0(i) + 1;
-		if (ct->degree(x) > max_arity) {
-			max_arity = ct->degree(x);
-		}
-	}
-	return max_arity;
-}
-
-template <class type>
-double get_average_arity(type * ct) {
-	size_t x;
-	size_t degree_node = ct->degree(1);
-	double sum_arity = degree_node; 
-	size_t nodes = 1;
-	for (size_t i=1; i<ct->count_nodes(); i++) {
-		x = ct->tree_select0(i) + 1;
-		degree_node = ct->degree(x);
-		if (degree_node > 0) {
-			sum_arity += degree_node;
-			nodes++;
-		}
-	}
-	return sum_arity / nodes;
-}
-
-
-// tree_height: return the max tree height.
-template <class type>
-size_t get_tree_height(type * ct, size_t node=1, size_t height=0) {
-	vector <size_t> height_list;
-	size_t node_degree = ct->degree(node);
-	size_t new_node;
-	if (node_degree == 0) return height;
-	else {
-		height++;
-		for (size_t i=0; i<node_degree; i++) {
-			new_node = ct->child(node, i+1);
-			height_list.push_back(get_tree_height(ct, new_node, height));
-		}
-	}
-	return *max_element(height_list.begin(), height_list.end());
-}
-
-// average_tree_height: return the average tree height.
-template <class type>
-double get_tree_average_height(type * ct, double* sum_heights, double* count_nodes, size_t node=1, size_t height=0) {
-	size_t node_degree = ct->degree(node);
-	size_t new_node;
-	if (node_degree == 0) {
-		*sum_heights += height;
-		(*count_nodes)++;
-		return height;
-	}
-	else {
-		height++;
-		for (size_t i=0; i<node_degree; i++) {
-			new_node = ct->child(node, i+1);
-			get_tree_average_height(ct, sum_heights, count_nodes, new_node, height); 
-		}
-	}
-	return *sum_heights / *count_nodes;
-}
-
-// check_balanced: return true if bp contain a balanced sequence of parentheses.
-bool check_balanced(bit_vector *bp) {
-	stack<char> bp_stack;
-	size_t N = bp->size(); 
-	for (size_t i=0; i<N; i++) {
-		if ((*bp)[i] == 1u) bp_stack.push('1');
-		else
-			if (bp_stack.empty() == false) bp_stack.pop();
-			else return false;
-	}
-
-	if (bp_stack.empty() == true) return true;
-	return false;
-}
-
-int main(int argc, const char *argv[])
-{
-	string str, str1, str2;
-	str1 = "()";
-	str = "(";
-	str1 += multiply_str(str, 10) + ")";
-	cout << str1 << endl;
+int main(int argc, const char *argv[]) {
+	ofstream out;
+	out.open("test.txt");
+	write_file(out, 0);
 	return 0;
 }
--- src/tree_generator/tree_generator.cpp
+++ src/tree_generator/tree_generator.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <fstream>
 
+/*
 #define MIN_DEGREE 900
 #define MARGIN_DEGREE 124
 #define MIN_HEIGHT 30
 #define MARGIN_HEIGHT 15
 #define MAX_HEIGHT 50
+*/
+#define MIN_DEGREE 900
+#define MARGIN_DEGREE 100
+#define MIN_HEIGHT 3
+#define MARGIN_HEIGHT 1
+#define MAX_HEIGHT 5
 using namespace std;
 
 string multiply_str(string a, size_t n) {
@@ -16,7 +23,7 @@ string multiply_str(string a, size_t n) {
 
 void gen(string& bp, size_t current_height) {
 	if (current_height  == MIN_HEIGHT) {
-		current_height += rand() % MARGIN_HEIGHT + 1;
+		current_height += rand() % MARGIN_HEIGHT;
 	}
 	current_height++;
 	string bp_tmp = "";
@@ -41,6 +48,7 @@ bool check_bp(string bp) {
 		if (bp[i] == '(') open++;
 		else close++;
 	}
+	cout << "nodes: " << open << endl;
 	return open == close;
 }
 
@@ -51,10 +59,36 @@ void write_bp(string name_file, string bp) {
 	out_file.close();
 }
 
+void random_tree_to_file(string& bp, size_t current_height, ofstream& out_file) {
+
+	if (current_height  == MIN_HEIGHT) {
+		current_height += rand() % MARGIN_HEIGHT + 1;
+	}
+	current_height++;
+	string bp_tmp = "";
+	if (current_height > MAX_HEIGHT) {
+		out_file << ")";
+		return;
+	}
+	else {
+		size_t degree = rand() % MARGIN_DEGREE + MIN_DEGREE;
+		bp_tmp = multiply_str("(", degree) + ")";
+		out_file << bp_tmp;
+		for (size_t i=0; i<degree; i++) {
+			random_tree_to_file(bp, current_height, out_file);
+		}
+	}
+	//out_file.close();
+}
+
 int main(int argc, const char *argv[])
 {
+	//ofstream out_file;
+	//out_file.open("giant_random_tree.ascii");
+
 	string bp = "(";
 	gen(bp, 1);
 	if (check_bp(bp) == true) 	write_bp("giant_random_tree.ascii", bp);
+	//random_tree_to_file(bp, 1, out_file);
 	return 0;
 }
