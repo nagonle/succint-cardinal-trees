ebc6847ff35b5eab6cec21cf4bfca1f8bd7f4123
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2017-01-08 10:26:32 -0300

    fix wt, wth. update on scripts

--- src/scripts/script_order_by_structure.sh
+++ src/scripts/script_order_by_structure.sh
@@ -13,18 +13,18 @@
 ./ct_main bs /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
 
 ./ct_main wt /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
-./ct_main wt /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main wt32 /data/Trees/DFUDS/LZ78/English/englishDFUDS
 ./ct_main wt /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
-./ct_main wt /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main wt32 /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
 ./ct_main wt /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
-./ct_main wt /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main wt32 /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
 
 ./ct_main wth /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/English/englishDFUDS
+./ct_main wth32 /data/Trees/DFUDS/LZ78/English/englishDFUDS
 ./ct_main wth /data/Trees/DFUDS/LZ78/DNA/dnaDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
+./ct_main wth32 /data/Trees/DFUDS/LZ78/Sources/sourcesDFUDS
 ./ct_main wth /data/Trees/DFUDS/LZ78/XML/xmlDFUDS
-./ct_main wth /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
+./ct_main wth32 /data/Trees/DFUDS/LZ78/MIDI/midiDFUDS
 
 ./ct_main gmr /data/Trees/DFUDS/LZ78/Prots/protsDFUDS
 ./ct_main gmr /data/Trees/DFUDS/LZ78/English/englishDFUDS
